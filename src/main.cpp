531071e73ccbf2cda4e08631b02d19b67fa67619
Steven Yik <yiksteve@msu.edu>
2021-06-04 14:29:42 -0400

    Refactor data saving.

--- src/main.cpp
+++ src/main.cpp
@@ -18,26 +18,27 @@ std::string m_depthFileDirectory = "";
 std::string m_colorFileDirectory = "";
 std::string m_irFileDirectory = "";
 
+
 int mkdir_p(const char *path)
 {
-    /* Adapted from http://stackoverflow.com/a/2336245/119527 */
+    // Adapted from http://stackoverflow.com/a/2336245/119527 
     const size_t len = strlen(path);
     char _path[PATH_MAX];
     char *p; 
 
     errno = 0;
 
-    /* Copy string so its mutable */
+    // Copy string so its mutable 
     if (len > sizeof(_path)-1) {
         errno = ENAMETOOLONG;
         return -1; 
     }   
     strcpy(_path, path);
 
-    /* Iterate the string */
+    // Iterate the string 
     for (p = _path + 1; *p; p++) {
         if (*p == '/') {
-            /* Temporarily truncate */
+            // Temporarily truncate 
             *p = '\0';
 
             if (mkdir(_path, S_IRWXU) != 0) {
@@ -133,6 +134,37 @@ void try_save_calibration(const char* base_dir, k4a_device_t device){
     }
 }
 
+void save_color_image(k4a_image_t image, std::string dir, std::string frame_s){
+
+    // code for storing the buffer in an image file
+    uint8_t* image_buffer = k4a_image_get_buffer(image);
+    size_t image_buffer_size = k4a_image_get_size(image);
+    std::string file_name = dir;
+    file_name += frame_s;
+    file_name += ".jpeg";
+    std::ofstream file_object(file_name, std::ios::out| std::ios::binary);
+    
+    file_object.write(reinterpret_cast<char*>(image_buffer), 
+        image_buffer_size);
+    file_object.flush();
+    file_object.close();
+}
+
+void save_depth_or_ir_image(k4a_image_t image, std::string dir, std::string frame_s){
+    // code for storing the buffer in an image file
+    uint8_t* image_buffer = k4a_image_get_buffer(image);
+    size_t image_buffer_size = k4a_image_get_size(image);
+    std::string file_name = dir;
+    file_name += frame_s;
+    file_name += ".bin";
+    std::ofstream file_object(file_name, std::ios::out| std::ios::binary);
+    
+    file_object.write(reinterpret_cast<char*>(image_buffer), 
+        image_buffer_size);
+    file_object.flush();
+    file_object.close();  
+}
+
 int main(int argc, char* argv[]) {
 
     std::string s = date::format("%m_%d_%Y", std::chrono::system_clock::now());
@@ -181,11 +213,10 @@ int main(int argc, char* argv[]) {
     // TODO: Change this to a config file later.
     k4a_device_configuration_t config = K4A_DEVICE_CONFIG_INIT_DISABLE_ALL;
     config.color_format = K4A_IMAGE_FORMAT_COLOR_MJPG;
-    //config.color_resolution = K4A_COLOR_RESOLUTION_1440P;
-    config.color_resolution = K4A_COLOR_RESOLUTION_3072P;
-    //config.depth_mode = K4A_DEPTH_MODE_WFOV_2X2BINNED;
-    config.depth_mode = K4A_DEPTH_MODE_WFOV_UNBINNED;
-    config.camera_fps = K4A_FRAMES_PER_SECOND_15;
+    config.color_resolution = K4A_COLOR_RESOLUTION_2160P;
+    //config.color_resolution = K4A_COLOR_RESOLUTION_3072P;
+    config.depth_mode = K4A_DEPTH_MODE_WFOV_2X2BINNED;
+    config.camera_fps = K4A_FRAMES_PER_SECOND_30;
     // about returning only synchronized images
     // https://microsoft.github.io/Azure-Kinect-Sensor-SDK/master/structk4a__device__configuration__t_a8208974f05d89fc1362c6a0900bdef4d.html#a8208974f05d89fc1362c6a0900bdef4d
     
@@ -198,10 +229,9 @@ int main(int argc, char* argv[]) {
 
     long last_timestamp = 0;
     int frames_captured = 0;
+    std::string frame_s = "";
     while (frames_captured++ < captureFrameCount)
     {
-        k4a_image_t image;
-
         // Get a depth frame
         switch (k4a_device_get_capture(device, &capture, TIMEOUT_IN_MS))
         {
@@ -216,100 +246,32 @@ int main(int argc, char* argv[]) {
             return returnCode;
         }
 
-        std::string frame_s = std::to_string(frames_captured);
+        frame_s = std::to_string(frames_captured);
         printf("Capture");
 
         // Probe for a color image
-        image = k4a_capture_get_color_image(capture);
-    
-        if (image)
-        {
-            long new_timestamp = (long)k4a_image_get_device_timestamp_usec(image);
+        k4a_image_t color_image = k4a_capture_get_color_image(capture);
+        // probe for a IR16 image
+        k4a_image_t ir_image = k4a_capture_get_ir_image(capture);
+        // Probe for a depth16 image
+        k4a_image_t depth_image = k4a_capture_get_depth_image(capture);
+
+        if (color_image != NULL && ir_image != NULL && depth_image != NULL){
+            long new_timestamp = (long)k4a_image_get_device_timestamp_usec(color_image);
             auto time_diff = (double)(new_timestamp - last_timestamp)/1000.0;
             last_timestamp = new_timestamp;
-            printf(" | Color res:%4dx%4d stride:%5d %f",
-                   k4a_image_get_height_pixels(image),
-                   k4a_image_get_width_pixels(image),
-                   k4a_image_get_stride_bytes(image),
-                   time_diff);
+            printf(" | Time between captures: %f(ms)\n",
+                    time_diff);
 
-            // code for storing the buffer in an image file
-            uint8_t* image_buffer = k4a_image_get_buffer(image);
-            size_t image_buffer_size = k4a_image_get_size(image);
-            std::string file_name = m_colorFileDirectory;
-            file_name += frame_s;
-            file_name += ".jpeg";
-            std::ofstream file_object(file_name, std::ios::out| std::ios::binary);
-            
-            file_object.write(reinterpret_cast<char*>(image_buffer), 
-                image_buffer_size);
-            file_object.flush();
-            file_object.close();
-
-            k4a_image_release(image);
-        }
-        else
-        {
-            printf(" | Color None                       ");
+            // Save all the images
+            save_color_image(color_image,m_colorFileDirectory, frame_s);
+            save_depth_or_ir_image(ir_image, m_irFileDirectory, frame_s);
+            save_depth_or_ir_image(depth_image, m_depthFileDirectory, frame_s);
         }
 
-        // probe for a IR16 image
-        image = k4a_capture_get_ir_image(capture);
-        if (image != NULL)
-        {
-            printf(" | Ir16 res:%4dx%4d stride:%5d ",
-                   k4a_image_get_height_pixels(image),
-                   k4a_image_get_width_pixels(image),
-                   k4a_image_get_stride_bytes(image));
-            
-            // code for storing the buffer in an image file
-            uint8_t* image_buffer = k4a_image_get_buffer(image);
-            size_t image_buffer_size = k4a_image_get_size(image);
-            std::string file_name = m_irFileDirectory;
-            file_name += frame_s;
-            file_name += ".bin";
-            std::ofstream file_object(file_name, std::ios::out| std::ios::binary);
-            
-            file_object.write(reinterpret_cast<char*>(image_buffer), 
-                image_buffer_size);
-            file_object.flush();
-            file_object.close();            
-
-            k4a_image_release(image);
-        }
-        else
-        {
-            printf(" | Ir16 None                       ");
-        }
-
-        // Probe for a depth16 image
-        image = k4a_capture_get_depth_image(capture);
-        if (image != NULL)
-        {
-            printf(" | Depth16 res:%4dx%4d stride:%5d\n",
-                   k4a_image_get_height_pixels(image),
-                   k4a_image_get_width_pixels(image),
-                   k4a_image_get_stride_bytes(image));
-
-            // code for storing the buffer in an image file
-            uint8_t* image_buffer = k4a_image_get_buffer(image);
-            size_t image_buffer_size = k4a_image_get_size(image);
-            std::string file_name = m_depthFileDirectory;
-            file_name += frame_s;
-            file_name += ".bin";
-            std::ofstream file_object(file_name, std::ios::out| std::ios::binary);
-            
-            file_object.write(reinterpret_cast<char*>(image_buffer), 
-                image_buffer_size);
-            file_object.flush();
-            file_object.close();
-
-            k4a_image_release(image);
-        }
-        else
-        {
-            printf(" | Depth16 None\n");
-        }
+        if (color_image != NULL) k4a_image_release(color_image);
+        if (ir_image != NULL) k4a_image_release(ir_image);
+        if (depth_image != NULL) k4a_image_release(depth_image);
 
         // release capture
         k4a_capture_release(capture);
