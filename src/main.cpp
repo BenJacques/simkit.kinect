064d982c025b1504b89f9da770155727fdcb226c
Ben Jacques <ben.jacques@spindance.com>
2021-06-30 13:58:32 -0400

    Update to use Qt front-end

--- src/main.cpp
+++ src/main.cpp
@@ -19,99 +19,23 @@
 #include "buttons.h"
 #include "settings.h"
 
-static const char* root_dir = "/media/FarmData01/Datasets/";
-static kinect::Kinect kinect_device;
-static buttons::Buttons button_mapping;
-static settings::Settings curr_settings;
+#include "mainwindow.h"
+#include <QApplication>
 
-std::atomic <bool> temparature_logging_enabled (false);
-
-
-void logTemparatures(const char *fileDirectory){
-    temparature_logging_enabled = true;
-    std::string temparature_file_name = fileDirectory;
-    temparature_file_name += "/temparature_readings.csv";
-    std::ofstream file_object(temparature_file_name);
-    std::string header = "Timestamp,CPU Temp (C),GPU Temp (C),Kinect Temp (C)\n";
-    file_object.write(header.c_str(), header.length());
-
-    while (temparature_logging_enabled){
-        double cpu_temp;
-        double gpu_temp;
-        double kinect_temp;
-        bool nanoTempsRead = readTemparatures(cpu_temp, gpu_temp);
-        bool kinectTempRead = kinect_device.GetRecentTemparature(kinect_temp);
-        if (nanoTempsRead && kinectTempRead){
-            std::string s = date::format("%T", std::chrono::system_clock::now());
-            char *output;
-            int len = asprintf(&output, "%s,%f,%f,%f\n",s.c_str(),cpu_temp,gpu_temp,kinect_temp);
-            file_object.write(output,len);
-            free(output);
-            file_object.flush();
-        }
-
-        usleep(1*1000*1000); // Log every 60 seconds
-    }
-    file_object.flush();
-    file_object.close();
-}
 
 
 int main(int argc, char* argv[]) {
     UNUSED_PARAMS(argc, argv);
-    // Set up GPIO pins on the Nano
-    button_mapping.SetupPins();
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
 
-    std::thread button_thread = button_mapping.PollInputsThread(); 
+    return a.exec();
+
+    /*
+
     
-    // Create the directory structure for saving data
-    std::string s = date::format("%m_%d_%Y", std::chrono::system_clock::now());
-    printf("Time: %s\n", s.c_str());
-
-    if (dirExists(root_dir) ==false){
-        printf("Error! No External Hard Drive detected.\nWould you like to store data locally?.\n");
-        while (true){
-            if (button_mapping.yes_button_clicked){
-                printf("Yes button clicked.");
-                button_mapping.yes_button_clicked = false;
-                root_dir = "~/Documents/Datasets/";
-                break;
-            }
-            if (button_mapping.no_button_clicked){
-                printf("No button clicked.");
-                button_mapping.no_button_clicked = false;
-                return -1; //Exit the application
-            }
-        }
-    }
-
-    //Create the needed directories and load settings
     
-    std::string base_dir = root_dir;
-    base_dir += s;
-    printf("Creating data save directories at %s\n", base_dir.c_str());
-    if (curr_settings.CreateDataCaptureDirectories(base_dir.c_str()) == false){
-        return -1;
-    }
-
-    printf("Loading settings...\n");
-    if (curr_settings.LoadSettings("config.json") == false){
-        return -1;
-    }
-
-
-    // Connect to the Kinect 
-    if (kinect_device.Connect(-1, curr_settings) == false){
-        printf("Failed to connect to Kinect.\n");
-        return 2;
-    }
-
-    // Try saving the calibration file if possible
-    kinect_device.TrySaveCalibrationFile(base_dir.c_str());
-
-
-    std::thread log_temps_thread (logTemparatures, base_dir.c_str());
-    std::thread kinect_stream_thread;
     printf("Waiting for button press to start...\n");
     while (true){
         if (button_mapping.start_button_clicked){
@@ -140,29 +64,7 @@ int main(int argc, char* argv[]) {
             break;
         }
         
-        // This uses a system call to open a recent image from the color directory to show what the camera 
-        // is looking at. I don't have a way to automatically close it yet, so I need to leave it off for now.
-        /* if (button_mapping.yes_button_clicked)
-        {
-            printf("Yes button clicked.\n");
-            button_mapping.yes_button_clicked = false;
-            char* image_to_show;
-            int image_num = kinect_device.imageCount -1;
-            int len = asprintf(&image_to_show, "eog %s%d.jpeg", curr_settings.data_dirs.colorFileDirectory.c_str(),image_num);
-            UNUSED_PARAM(len);
-            printf("Opening image %s\n", image_to_show);
-            if (system(image_to_show) == 0){
-                // Do nothing
-            }
-        } */
 
-        // This is only here because the exit button is not working on my setup
-        /* if (button_mapping.no_button_clicked)
-        {
-            printf("Exit button clicked.\n");
-            button_mapping.no_button_clicked = false;
-            break;
-        } */
     }
 
     kinect_device.Stop();
@@ -179,7 +81,7 @@ int main(int argc, char* argv[]) {
     printf("**LOG TEMPS THREAD DONE****\n");
 
     kinect_device.Stop();
-
+ */
 
     return 0;
 }
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,6 +1,22 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <fstream>
+#include <QMessageBox>
+#include <unistd.h>
+
+#include "common_macros.h"
+#include "date.h"
+#include "utils.h"
+#include "kinect.h"
+#include "buttons.h"
+#include "settings.h"
+
+
+std::atomic <bool> temparature_logging_enabled (false);
+
+
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -11,4 +27,188 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::logTemparatures(){
+    const char *fileDirectory = base_dir.c_str();
+    temparature_logging_enabled = true;
+    std::string temparature_file_name = fileDirectory;
+    temparature_file_name += "/temparature_readings.csv";
+    std::ofstream file_object(temparature_file_name);
+    std::string header = "Timestamp,CPU Temp (C),GPU Temp (C),Kinect Temp (C)\n";
+    file_object.write(header.c_str(), header.length());
+
+    while (temparature_logging_enabled){
+        double cpu_temp;
+        double gpu_temp;
+        double kinect_temp;
+        bool nanoTempsRead = readTemparatures(cpu_temp, gpu_temp);
+        bool kinectTempRead = kinect_device.GetRecentTemparature(kinect_temp);
+        if (nanoTempsRead && kinectTempRead){
+            std::string s = date::format("%T", std::chrono::system_clock::now());
+            char *output;
+            int len = asprintf(&output, "%s,%f,%f,%f\n",s.c_str(),cpu_temp,gpu_temp,kinect_temp);
+            file_object.write(output,len);
+            free(output);
+            file_object.flush();
+        }
+
+        usleep(1*1000*1000); // Log every 60 seconds
+    }
+    file_object.flush();
+    file_object.close();
+}
+
+void MainWindow::initializeUi()
+{
+    root_dir = "/media/FarmData01/Datasets/";
+    
+    // Set up GPIO pins on the Nano
+    button_mapping.SetupPins();
+    button_thread = button_mapping.PollInputsThread(); 
+
+    // Create the directory structure for saving data
+    std::string s = date::format("%m_%d_%Y-%H_%M_%S", std::chrono::system_clock::now());
+    printf("Time: %s\n", s.c_str());
+
+    if (dirExists(root_dir) ==false){
+        QMessageBox msgBox;
+        msgBox.setText("No external hard drive detected.\nWould you like to store data locally instead?");
+        msgBox.setInformativeText("Save locally?");
+        msgBox.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
+        msgBox.setDefaultButton(QMessageBox::Yes);
+        int retVal = msgBox.exec();
+        if (retVal == QMessageBox::Yes){
+            root_dir = "~/Documents/Datasets/";
+        }
+        else {
+            return;
+        }
+    }
+
+    //Create the needed directories and load settings
+    base_dir = root_dir;
+    base_dir += s;
+    printf("Creating data save directories at %s\n", base_dir.c_str());
+    if (curr_settings.CreateDataCaptureDirectories(base_dir.c_str()) == false){
+        return;
+    }
+
+    printf("Loading settings...\n");
+    if (curr_settings.LoadSettings("config.json") == false){
+        return;
+    }
+
+
+    // Connect to the Kinect 
+    if (kinect_device.Connect(-1, curr_settings) == false){
+        printf("Failed to connect to Kinect.\n");
+        return;
+    }
+
+    // Try saving the calibration file if possible
+    kinect_device.TrySaveCalibrationFile(base_dir.c_str());
+
+
+    log_temps_thread = std::thread(&MainWindow::logTemparatures, this);
+
+}
+
+void MainWindow::showEvent(QShowEvent* ev)
+{
+    QMainWindow::showEvent(ev);
+    initializeUi();
+}
+
+
+void MainWindow::on_pushButton_Start_clicked()
+{
+    if (kinect_stream_thread.joinable() == false) {
+        handleKinectStart();
+    }
+    else {
+        handleKinectStop();
+    }
+}
+
+void MainWindow::on_actionStart_triggered()
+{
+    if (kinect_stream_thread.joinable() == false) {
+        handleKinectStart();
+    }
+    else {
+        handleKinectStop();
+    }
+}
+
+void MainWindow::on_pushButton_Config_clicked()
+{
+
+}
+
+void MainWindow::on_actionConfig_triggered()
+{
+
+}
+
+void MainWindow::on_actionExit_triggered()
+{
+    handleExit();
+}
+
+void MainWindow::on_pushButton_Exit_clicked()
+{
+    handleExit();
+}
+
+void MainWindow::on_actionVersion_triggered()
+{
+
+}
+
+void MainWindow::handleKinectStart()
+{
+    if (kinect_stream_thread.joinable() == false){
+        ui->pushButton_Start->setText("Stop Streaming");
+        kinect_stream_thread = kinect_device.RunThread();
+    }
+}
+
+void MainWindow::handleKinectStop()
+{
+    kinect_device.Stop();
+    if (kinect_stream_thread.joinable()){
+        kinect_stream_thread.join();
+    }
+    ui->pushButton_Start->setText("Start Streaming");
+}
+
+void MainWindow::handleExit()
+{
+    QMessageBox msgBox;
+    msgBox.setText("Are you sure you want to exit?");
+    msgBox.setInformativeText("Exit?");
+    msgBox.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
+    msgBox.setDefaultButton(QMessageBox::No);
+    int retVal = msgBox.exec();
+    if (retVal == QMessageBox::No){
+        return;
+    }
+
+
+    
+    if (kinect_stream_thread.joinable()){
+        kinect_device.Stop();
+        kinect_stream_thread.join();
+    }
+
+    printf("**KINECT THREAD DONE****\n");; 
+    temparature_logging_enabled = false;
+    button_mapping.Close();
+    button_thread.join();
+    printf("**BUTTON THREAD DONE****\n");
+    log_temps_thread.join();
+    printf("**LOG TEMPS THREAD DONE****\n");
+    QCoreApplication::exit();
 }
