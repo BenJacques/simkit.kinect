6a80c85f24c16409da30823440906f48e70abe9a
Ben Jacques <ben.jacques@spindance.com>
2021-07-07 10:17:35 -0400

    Update build script to use current user documents folder.

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ MY_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd $MY_DIR
 mkdir -p build
 cd build
-cmake .. "-DCMAKE_TOOLCHAIN_FILE=/home/animallab/Documents/repos/vcpkg/scripts/buildsystems/vcpkg.cmake"
-make
+cmake .. "-DCMAKE_TOOLCHAIN_FILE=~/Documents/repos/vcpkg/scripts/buildsystems/vcpkg.cmake"
+make -j2
